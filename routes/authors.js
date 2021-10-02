5445b4b59d8ea6842087dd34f28593cd5e3168c5
unknown <m13545022224@gmail.com>
2021-10-02 14:02:29 +0800

    Initial Setup

---
+++ routes/authors.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const author = require('../models/author')
+const router = express.Router()
+const Author = require('../models/author')
+
+//All Authors Route
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null&& req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find({searchOptions})
+        res.render('authors/index', {
+            author: authors,
+            searchOptions: req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
+})
+
+//New Author Route
+router.get('./new', (req, res) => {
+    res.render('authors/new', {author: new Author()})
+})
+
+//Create Author Route
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        // res.redirect('authors/${newAuthor.id}')
+        res.redirect('authors')
+    } catch{
+        res.render('author/new',{
+        author:author,
+        errorMessage:'Error creating Author'
+        })
+    }
+    // author.save((err, newAuthor) => {
+    //     if(err){
+    //         res.render('author/new',{
+    //             author:author,
+    //             errorMessage:'Error creating Author'
+    //         })
+    //     } else {
+    //         // res.redirect('authors/${newAuthor.id}')
+    //         res.redirect('authors')
+    //     }
+    // })
+    // res.send(req.body.name)
+})
+
+module.exports = router
